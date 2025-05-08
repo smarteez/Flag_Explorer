015d3847b5f7189360c3e65f8ed8f7055bb597e7
Donny Singh <d.singh.zn@gmail.com>
2025-05-08 15:49:20 +0200

    added endpoint to appsettings.json

--- Flag_Explorer.API/obj/Debug/net9.0/Flag_Explorer.API.AssemblyInfo.cs
+++ Flag_Explorer.API/obj/Debug/net9.0/Flag_Explorer.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flag_Explorer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c90d2d300e90bd4a04dceb54efef0513d8d6fa78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flag_Explorer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flag_Explorer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
