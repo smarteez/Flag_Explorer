86f4343df309d035f431af0d1894813b0f9abf0a
Donny Singh <d.singh.zn@gmail.com>
2025-05-08 16:04:23 +0200

    Create a DataLayer and added the Entities to match the api data

---
+++ Flag Explorer/Flag_Explorer.Data/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flag_Explorer.Data.Entities
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string OfficialName { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public List<string> Borders { get; set; }
+        public long Population { get; set; }
+        public Dictionary<string, string> Languages { get; set; }
+        public string Flag { get; set; }
+        public Dictionary<string, Currency> Currencies { get; set; }
+
+    }
+}
--- Flag_Explorer.API/obj/Debug/net9.0/Flag_Explorer.API.AssemblyInfo.cs
+++ Flag_Explorer.API/obj/Debug/net9.0/Flag_Explorer.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flag_Explorer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef72825bd98d46c92229c37be0221df6494f922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4db06579302495a1f47b8e4a979ad94748fe41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flag_Explorer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flag_Explorer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
