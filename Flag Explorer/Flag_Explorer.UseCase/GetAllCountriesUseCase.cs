34ec71c5825e31c5e07b7760b24a76552d89b11c
Donny Singh <d.singh.zn@gmail.com>
2025-05-08 16:51:04 +0200

    added the UseCases

--- Flag Explorer/Flag_Explorer.UseCase/GetAllCountriesUseCase.cs
+++ Flag Explorer/Flag_Explorer.UseCase/GetAllCountriesUseCase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Flag_Explorer.Domain.DTO;
+using Flag_Explorer.Repository.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,28 @@ namespace Flag_Explorer.UseCase
 {
     public class GetAllCountriesUseCase
     {
+        private readonly ICountryRepository ICountryRepository;
+
+        public GetAllCountriesUseCase(ICountryRepository countryRepository)
+        {
+            ICountryRepository = countryRepository ?? throw new ArgumentNullException(nameof(countryRepository));
+        }
+
+        public async Task<List<CountryDTO?>> ExecuteAsync()
+        {
+            try
+            {
+                var countries = await ICountryRepository.GetAllCountriesAsync();
+                if (countries == null)
+                {
+                    throw new Exception("The country list returned is null.");
+                }
+                return countries.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while fetching countries: {ex.Message}", ex);
+            }
+        }
     }
 }
--- Flag Explorer/Flag_Explorer.UseCase/GetByNameCountryUseCase.cs
+++ Flag Explorer/Flag_Explorer.UseCase/GetByNameCountryUseCase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Flag_Explorer.Domain.DTO;
+using Flag_Explorer.Repository.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,30 @@ namespace Flag_Explorer.UseCase
 {
     public class GetByNameCountryUseCase
     {
+        private readonly ICountryRepository ICountryRepository;
+        public GetByNameCountryUseCase(ICountryRepository countryRepository)
+        {
+            ICountryRepository = countryRepository;
+        }
+        public async Task<CountryDetailsDTO?> Execute(string countryName)
+        {
+            if (string.IsNullOrWhiteSpace(countryName))
+            {
+                throw new ArgumentException("Country name cannot be null or empty.", nameof(countryName));
+            }
+            try
+            {
+                var countryDetails = await ICountryRepository.GetCountryByNameAsync(countryName);
+                if (countryDetails == null)
+                {
+                    throw new Exception($"Country with name {countryName} not found.");
+                }
+                return countryDetails;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while fetching country details: {ex.Message}", ex);
+            }
+        }
     }
 }
